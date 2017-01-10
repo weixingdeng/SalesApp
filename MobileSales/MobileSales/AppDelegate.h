08d958b88cb1a5d1ae8d4bde5d8af7bf729b1a75
@dwx <455613844@qq.com>
2017-01-10 13:22:57 +0800

    new project

---
+++ MobileSales/MobileSales/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileSales
+//
+//  Created by dengwx on 17/1/10.
+//  Copyright © 2017年 wxdeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
