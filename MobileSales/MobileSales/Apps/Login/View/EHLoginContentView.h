d79db7b699d0a77a222471c9ea8933da913a7d9d
李旭 <lixu@DennisLeesMac.local>
2017-01-16 13:43:50 +0800

    添加登录内容视图

---
+++ MobileSales/MobileSales/Apps/Login/View/EHLoginContentView.h
@@ -0,0 +1,13 @@
+//
+//  EHLoginContentView.h
+//  MobileSales
+//
+//  Created by 李旭 on 17/1/16.
+//  Copyright © 2017年 wxdeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EHLoginContentView : UIView
+
+@end
---
+++ MobileSales/MobileSales/Apps/Login/View/EHLoginContentView.m
@@ -0,0 +1,21 @@
+//
+//  EHLoginContentView.m
+//  MobileSales
+//
+//  Created by 李旭 on 17/1/16.
+//  Copyright © 2017年 wxdeng. All rights reserved.
+//
+
+#import "EHLoginContentView.h"
+
+@implementation EHLoginContentView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
